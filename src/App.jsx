830e4597472fc0225106aca7328ce38584a4009e
praneet <praneetkashyap001@gmail.com>
2024-06-06 20:04:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Landing from './components/Landing'
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/' element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
