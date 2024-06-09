71a0f81fa209e188e6b820e427c4cf1880d1ee32
praneet <praneetkashyap001@gmail.com>
2024-06-09 14:49:17 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Landing from './components/Landing'
 
 function App() {
-
+  const navigate = useNavigate()
   return (
     <div>
+      <div>
+        <button onClick={() => { navigate("/") }}>Landing Page</button>
+        <button onClick={() => { navigate("/dashboard") }}>Dashboard</button>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path='/dashboard' element={<Dashboard />} />
