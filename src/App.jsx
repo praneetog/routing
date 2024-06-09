c9b2344cb8b7d62a89b4973c2c0c4f2f8b64ab41
praneet <praneetkashyap001@gmail.com>
2024-06-09 15:10:51 +0530

    new commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, useNavigate, Navigate } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
-import Landing from './components/Landing'
+// import Dashboard from './components/Dashboard'
+// import Landing from './components/Landing'
+import React from 'react'
+
+const Dashboard = React.lazy(() => import("./components/Dashboard"))
+const Landing = React.lazy(() => import("./components/Landing"))
 
 function App() {
-  const navigate = useNavigate()
   return (
     <div>
-      <div>
-        <button onClick={() => { navigate("/") }}>Landing Page</button>
-        <button onClick={() => { navigate("/dashboard") }}>Dashboard</button>
-      </div>
       <BrowserRouter>
+        <Appbar />
         <Routes>
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/' element={<Landing />} />
         </Routes>
       </BrowserRouter>
+    </div>
+  )
+}
+
+function Appbar() {
+  const navigate = useNavigate()
+  return(
+    <div>
+      <button onClick={() => { navigate("/") }}>Landing Page</button>
+      <button onClick={() => { navigate("/dashboard") }}>Dashboard</button>
     </div>
   )
 }
